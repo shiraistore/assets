12424f605710cc7630275ce7d26c320e57644a62
kensukematsunaga <kensuke-matsunaga@shirai-s.co.jp>
2023-08-02 16:16:21 +0900

    chenged files

---
+++ js/script-receipt.js
@@ -0,0 +1,89 @@
+$(function () {
+    inputValue();
+    receiptFormValidate();
+});
+
+function inputValue(){
+    const code = getParam(no);
+    document.getElementById("orderCode").value = code;
+}
+
+function getParam(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, '\\$&');
+	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function receiptFormValidate() {
+    $('#receiptForm form').validate({
+        rules: {
+            field_24437_sei: {
+                required: true
+            },
+            field_24437_mei: {
+                required: true
+            },
+            field_24438: {
+                required: true,
+                email: true
+            },
+            field_24443: {
+                required: true
+            },
+            field_24455: {
+                required: true
+            },
+            
+            field_4559439_sei: {
+                required: true
+            },
+            field_4559439_mei: {
+                required: true
+            },
+            field_4552323: {
+                required: true,
+                email: true
+            },
+            field_4553669: {
+                required: true
+            },
+        },
+
+        messages: {
+            field_24437_sei: {
+                required: '姓を入力してください'
+            },
+            field_24437_mei: {
+                required: '名を入力してください'
+            },
+            field_24438: {
+                required: 'メールアドレスを入力してください',
+                email: 'メールアドレスを正確に入力してください'
+            },
+            field_24443: {
+                required: '内容を入力してください'
+            },
+            field_24455: {
+                required: '選択してください'
+            },
+
+            field_4559439_sei: {
+                required: '姓を入力してください'
+            },
+            field_4559439_mei: {
+                required: '名を入力してください'
+            },
+            field_4552323: {
+                required: 'メールアドレスを入力してください',
+                email: 'メールアドレスを正確に入力してください'
+            },
+            field_4553669: {
+                required: '宛名を入力してください'
+            }
+        }
+    });
+}
