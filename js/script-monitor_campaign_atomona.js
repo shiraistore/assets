d0eb0f75046a043f18863f382e310dffbcee3457
kensukematsunaga <kensuke-matsunaga@shirai-s.co.jp>
2025-02-10 08:23:05 +0900

    changed files

--- js/script-monitor_campaign_atomona.js
+++ js/script-monitor_campaign_atomona.js
@@ -3,8 +3,42 @@ $(function () {
 });
 
 function monitor_campaign_atomona_validate() {
-    $("input[name='field_5134815[]']").on("change", function () {
-        if ($("input[name='field_5134815[]']:checked").length > 3) {
+        // 選択肢の変更時に制御
+        $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").on("change", function () {
+            updateSelectOptions();
+            checkProductSelection();
+            $(this).removeClass('error');
+        });
+
+        $("input[name='field_5134817_0'], input[name='field_5134817_1']").on('change', function () {
+            checkCompanyPopularity();
+        });
+    
+        function updateSelectOptions() {
+            let selectedValues = [];
+            $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").each(function () {
+                let val = $(this).val();
+                if (val !== '' && val !== '4') {
+                    selectedValues.push(val);
+                }
+            });
+    
+            $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").each(function () {
+                let currentVal = $(this).val();
+                let $this = $(this);
+    
+                $this.find("option").prop("disabled", false);
+    
+                $this.find('option').each(function () {
+                    if (selectedValues.includes($(this).val()) && $(this).val() !== currentVal && $(this).val() !== '4') {
+                        $(this).prop('disabled', true);
+                    }
+                });
+            });
+        }
+
+    $("input[name='field_5134815']").on("change", function () {
+        if ($("input[name='field_5134815']:checked").length > 3) {
             alert("最大3つまで選択可能です");
             this.checked = false;
         }
@@ -12,84 +46,91 @@ function monitor_campaign_atomona_validate() {
 
     $('#monitor_campaign_atomona form').validate({
         rules: {
-            field_5134805: {
-                required: true
-            },
-            field_5134791: {
-                required: true
-            },
-            field_5134806: {
-                required: true
-            },
-            field_5134807: {
-                required: true
-            },
-            field_5134815: {
-                required: true
-            },
-            field_5134790: {
-                required: true,
-                email: true
-            }
+            field_5134805: { required: true },
+            field_5134791: { required: true },
+            field_5134806: { required: true },
+            field_5134807: { required: true },
+            field_5134808: { required: true},
+            field_5134815: { required: true},
+            field_5134816: { required: true},
+            field_5134790: { required: true, email: true },
+            field_5141573: { required: true, depends: checkProductSelection },
+            field_5141575: { required: true, depends: checkProductSelection },
+            field_5141586: { required: true, depends: checkProductSelection },
+            field_5141587: { required: true, depends: checkProductSelection },
+            field_5134817_0: {required: true},
+            field_5134817_1: {required: true}
         },
         messages: {
-            field_5134805: {
-                required: '選択してください'
-            },
-            field_5134791: {
-                required: '選択してください'
-            },
-            field_5134806: {
-                required: '選択してください'
-            },
-            field_5134807: {
-                required: '選択してください'
-            },
-            field_5134815: {
-                required: '選択してください'
-            },
+            field_5134805: { required: '選択してください' },
+            field_5134791: { required: '選択してください' },
+            field_5134806: { required: '選択してください' },
+            field_5134807: { required: '選択してください' },
+            field_5134808: { required: '選択してください' },
+            field_5134815: { required: '選択してください（最大3つ）' },
+            field_5134816: { required: '選択してください' },
             field_5134790: {
-                required: '入力してください',
-                email: 'メールアドレスを正しく入力してください'
-            }
+                required: 'メールアドレスを入力してください',
+                email: '正しいメールアドレスを入力してください'
+            },
+            field_5141573: { required: '選択してください' },
+            field_5141575: { required: '選択してください' },
+            field_5141586: { required: '選択してください' },
+            field_5141587: { required: '選択してください' },
+            field_5134817_0: { required: '' },
+            field_5134817_1: { required: '' }
         },
         errorPlacement: function (error, element) {
+            if (element.is("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']")) {
+                return;
+            } else if (element.is("input[name='field_5134817_0'], input[name='field_5134817_1']")){
+                $('.company-popularity-error').html('<label class="error">選択してください。</label>');
+            }
             element.closest('td').append(error);
+        },
+        submitHandler: function (form) {
+            if (!checkProductSelection()) {
+                return false;
+            } else if (!checkCompanyPopularity()){
+                return false;
+            }
+            form.submit();
+        }
+    });    
+}
+
+function checkProductSelection() {
+    let isAnyEmpty = false;
+    $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").each(function () {
+        if ($(this).val() === '') {
+            isAnyEmpty = true;
         }
     });
 
-    $(document).ready(function () {
-        // 選択肢の変更時に制御
-        $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").on("change", function () {
-            updateSelectOptions();
-        });
-    
-        function updateSelectOptions() {
-            // 全ての選択肢の値を取得
-            let selectedValues = [];
-            $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").each(function () {
-                let val = $(this).val();
-                if (val !== "" && val !== "4") { // 「希望しない」(value="4") は除外
-                    selectedValues.push(val);
-                }
-            });
-    
-            // 各セレクトボックスを更新
-            $("select[name='field_5141573'], select[name='field_5141575'], select[name='field_5141586'], select[name='field_5141587']").each(function () {
-                let currentVal = $(this).val();
-                let $this = $(this);
-    
-                // 一度すべての選択肢を有効にする
-                $this.find("option").prop("disabled", false);
-    
-                // 他のセレクトで選ばれている値を無効化
-                $this.find("option").each(function () {
-                    if (selectedValues.includes($(this).val()) && $(this).val() !== currentVal && $(this).val() !== "4") {
-                        $(this).prop("disabled", true);
-                    }
-                });
-            });
-        }
-    });
-    
+    let errorContainer = $('.product-selection-error');
+
+    if (isAnyEmpty) {
+        errorContainer.html('<label class="error">すべての希望商品を選択してください</label>');
+        return false;
+    } else {
+        errorContainer.html('');
+        return true;
+    }
+}
+
+function checkCompanyPopularity() {
+    let isField0Checked = $("input[name='field_5134817_0']:checked").length > 0;
+    let isField1Checked = $("input[name='field_5134817_1']:checked").length > 0;
+    let errorContainer = $('.company-popularity-error');
+
+    if (!isField0Checked && !isField1Checked) {
+        errorContainer.html('<label class="error">選択してください。</label>');
+        return false;
+    } else if (!isField0Checked || !isField1Checked) {
+        errorContainer.html('<label class="error">選択してください。</label>');
+        return false;
+    } else {
+        errorContainer.html('');
+        return true;
+    }
 }
